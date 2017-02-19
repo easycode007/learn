03a3f765d9abb11aeaaee157860e0f626e4103cc
Prometeus007 <easycode007@gmail.com>
2017-02-20 01:08:19 +0200

    added some modification on controller - but still not working

--- MovieProj/src/main/java/com/example/controller/MovieController.java
+++ MovieProj/src/main/java/com/example/controller/MovieController.java
@@ -1,8 +1,13 @@
 package com.example.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.model.Movie;
 import com.example.model.MovieDao;
@@ -14,15 +19,28 @@ public class MovieController {
 	
 	
 	@RequestMapping("/add")
-	public String addMovie(String name, String genre, 
-			String releaseDate, String shortDescription) {
+	public String addMovie(Model m, 
+			@RequestParam(value="name", required=true, defaultValue="DefaultName") String name, 
+			@RequestParam(value="genre", required=true, defaultValue="DefaultGenre") String genre, 
+			@RequestParam(value="releaseDate", required=true, defaultValue="ReleaseDate") String releaseDate,
+			@RequestParam(value="description", required=true, defaultValue="Description") String description) {
 		try {
-			Movie movie = new Movie(name, genre, releaseDate, shortDescription);
+			Movie movie = new Movie(name, genre, releaseDate, description);
 			movieDao.addMovie(movie);
 		} catch (Exception e) {
-			return "Error adding the movie " + e.toString();
+			m.addAttribute("error", e.toString());
+			return "error";
 		}
-		return "Movie successfully added";
+//		Map<String, Object> attributes = new HashMap<>();
+//		attributes.put("name", name);
+//		attributes.put("genre", genre);
+//		attributes.put("releaseDate", releaseDate);
+//		attributes.put("description", description);
+		m.addAttribute("name", name);
+		m.addAttribute("genre", genre);
+		m.addAttribute("releaseDate", releaseDate);
+		m.addAttribute("description", description);
+		return "success";
 	}
 	
 	@RequestMapping("/list")
---
+++ MovieProj/src/main/webapp/view/error.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
+</head>
+<body>
+    <h1> Error:  ${error} </h1>
+</body>
+</html>
