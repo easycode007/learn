25d474fb8c79aeb0fd56fc97317df7bb9fde584a
Prometeus007 <easycode007@gmail.com>
2017-02-24 02:43:38 +0200

    added controller for /list

--- MovieProj/src/main/java/com/example/controller/MovieController.java
+++ MovieProj/src/main/java/com/example/controller/MovieController.java
@@ -1,9 +1,19 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 //import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.model.Movie;
@@ -16,14 +26,14 @@ public class MovieController {
 	
 	
 	@RequestMapping("/add")
-	@ResponseBody
-	public String addMovie(String name, String genre) {
+//	@ResponseBody
+	public String addMovie(Model m, String name, String genre) {
 		try {
 //			Movie movie = new Movie(name, genre, releaseDate, description);
 			Movie movie = new Movie(name, genre);
 			movieDao.save(movie);
 		} catch (Exception e) {
-//			m.addAttribute("error", e.toString());
+			m.addAttribute("error", e.toString());
 			return "error";
 		}
 //		Map<String, Object> attributes = new HashMap<>();
@@ -31,21 +41,16 @@ public class MovieController {
 //		attributes.put("genre", genre);
 //		attributes.put("releaseDate", releaseDate);
 //		attributes.put("description", description);
-//		m.addAttribute("name", name);
-//		m.addAttribute("genre", genre);
+		m.addAttribute("name", name);
+		m.addAttribute("genre", genre);
 //		m.addAttribute("releaseDate", releaseDate);
 //		m.addAttribute("description", description);
 		return "success";
 	}
 	
-//	@RequestMapping("/list")
-//	public void listMovies() {
-//		
-//	}
-	
-//	@RequestMapping("/error")
-//	public String error() {
-//		
-//		return "";
-//	}
+	@RequestMapping("/list")
+	public @ResponseBody Iterable listMovies() {
+		Iterable<Movie> movies = movieDao.findAll();	
+		return movies;
+	}
 }
