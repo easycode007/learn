4ecb88eee7485cd1a739fbf698e95f1b6b773e27
Prometeus007 <easycode007@gmail.com>
2017-02-24 03:30:38 +0200

    some config for hsql db

--- MovieProj/src/main/java/com/example/config/DatabaseConfig.java
+++ MovieProj/src/main/java/com/example/config/DatabaseConfig.java
@@ -1,100 +1,70 @@
 package com.example.config;
 
+import java.io.IOException;
 import java.util.Properties;
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.hsqldb.Server;
+import org.hsqldb.persist.HsqlProperties;
+import org.hsqldb.server.ServerAcl.AclFormatException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.SmartLifecycle;
 
-//@Configuration
-//@EnableTransactionManagement
-public class DatabaseConfig {
-//	// Private fields
-//	  
-//	 @Autowired
-//	 private Environment env;
-//
-//	 @Autowired
-//	 private DataSource dataSource;
-//
-//	 @Autowired
-//	 private LocalContainerEntityManagerFactoryBean entityManagerFactory;
-//	 /**
-//	   * DataSource definition for database connection. Settings are read from
-//	   * the application.properties file (using the env object).
-//	   */
-//	 @Bean
-//	 public DataSource dataSource() {
-//		 DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//		 dataSource.setDriverClassName(env.getProperty("db.driver"));
-//		 dataSource.setUrl(env.getProperty("db.url"));
-//		 dataSource.setUsername(env.getProperty("db.username"));
-//		 dataSource.setPassword(env.getProperty("db.password"));
-//		 return dataSource;
-//	  }
-//
-//	  /**
-//	   * Declare the JPA entity manager factory.
-//	   */
-//	  @Bean
-//	  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//		  LocalContainerEntityManagerFactoryBean entityManagerFactory =
-//				  new LocalContainerEntityManagerFactoryBean();
-//	    
-//		  entityManagerFactory.setDataSource(dataSource);
-//	    
-//		  // Classpath scanning of @Component, @Service, etc annotated class
-//		  entityManagerFactory.setPackagesToScan(
-//				  env.getProperty("entitymanager.packagesToScan"));
-//	    
-//		  // Vendor adapter
-//		  HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-//		  entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
-//	    
-//		  // Hibernate properties
-//		  Properties additionalProperties = new Properties();
-//		  additionalProperties.put(
-//				  "hibernate.dialect", 
-//				  env.getProperty("hibernate.dialect"));
-//		  additionalProperties.put(
-//				  "hibernate.show_sql", 
-//				  env.getProperty("hibernate.show_sql"));
-//		  additionalProperties.put(
-//				  "hibernate.hbm2ddl.auto", 
-//				  env.getProperty("hibernate.hbm2ddl.auto"));
-//		  entityManagerFactory.setJpaProperties(additionalProperties);
-//	    
-//		  return entityManagerFactory;
-//	  }
-//
-//	  /**
-//	   * Declare the transaction manager.
-//	   */
-//	  @Bean
-//	  public JpaTransactionManager transactionManager() {
-//		  JpaTransactionManager transactionManager = 
-//				  new JpaTransactionManager();
-//		  transactionManager.setEntityManagerFactory(
-//				  entityManagerFactory.getObject());
-//		  return transactionManager;
-//	  }
-//	  
-//	  /**
-//	   * PersistenceExceptionTranslationPostProcessor is a bean post processor
-//	   * which adds an advisor to any bean annotated with Repository so that any
-//	   * platform-specific exceptions are caught and then rethrown as one
-//	   * Spring's unchecked data access exceptions (i.e. a subclass of 
-//	   * DataAccessException).
-//	   */
-//	  @Bean
-//	  public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-//		  return new PersistenceExceptionTranslationPostProcessor();
-//	  }
+public class DatabaseConfig implements SmartLifecycle {
+	private final Logger logger = LoggerFactory.getLogger(DatabaseConfig.class);
+	private HsqlProperties properties;
+	private Server server;
+	private boolean running = false;
+
+	public DatabaseConfig(Properties props) {
+		properties = new HsqlProperties(props);
+	}
+
+	@Override
+	public boolean isRunning() {
+		if (server != null)
+			server.checkRunning(running);
+		return running;
+	}
+
+	@Override
+	public void start() {
+		if (server == null) {
+			logger.info("Starting HSQL server...");
+			server = new Server();
+			try {
+				server.setProperties(properties);
+				server.start();
+				running = true;
+			} catch (AclFormatException afe) {
+				logger.error("Error starting HSQL server.", afe);
+			} catch (IOException e) {
+				logger.error("Error starting HSQL server.", e);
+			}
+		}
+	}
+
+	@Override
+	public void stop() {
+		logger.info("Stopping HSQL server...");
+		if (server != null) {
+			server.stop();
+			running = false;
+		}
+	}
+
+	@Override
+	public int getPhase() {
+		return 0;
+	}
+
+	@Override
+	public boolean isAutoStartup() {
+		return true;
+	}
+
+	@Override
+	public void stop(Runnable runnable) {
+		stop();
+		runnable.run();
+	}
 }
