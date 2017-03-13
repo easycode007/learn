f2f98090d099943d5bcb09f2394d3ff4ef7528dd
Prometeus007 <easycode007@gmail.com>
2017-03-14 00:13:25 +0200

    showmovies &tiles issues tried to fix

--- MovieProj/src/main/java/com/mov/controller/AppController.java
+++ MovieProj/src/main/java/com/mov/controller/AppController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -30,8 +31,8 @@ public class AppController {
 		return "home";
 	}
 	
-	@RequestMapping(value="/contactus")
-	public String contactUS() {
-		return "contactus";
+	@RequestMapping(value="/contactus", method = RequestMethod.GET)
+	public ModelAndView contactUS() {
+		return new ModelAndView("contactus");
 	}
 }
--- MovieProj/src/main/webapp/WEB-INF/view/tiles/template/menu.jsp
+++ MovieProj/src/main/webapp/WEB-INF/view/tiles/template/menu.jsp
@@ -1,8 +1,9 @@
 <nav>
 	<a href="/"><img class="logo" src="/imgs/Linux-icon.png"></a>
 	<ul id="menu">
-		<li><a href="/"> Home </a></li>
-       	<li><a href="/movie"> Add Movie </a></li>
-       	<li><a href="/contactus"> Contact Us </a></li>
+		<li> <a href="/"> Home </a> </li>
+       	<li> <a href="/movie"> Add Movie </a> </li>
+       	<li> <a href="/list"> Show Movies </a> </li>
+       	<li> <a href="/contactus"> Contact Us </a> </li>
 	</ul>
 </nav>
