1d6c05b4a46af6a01719b0836b398d1212d8028a
Stan Alexandru Razvan <Razvan.Stan@endava.com>
2017-05-25 15:14:58 +0300

    added betProject

---
+++ betApp/src/main/java/com/bet/betApp/BetAppApplication.java
@@ -0,0 +1,12 @@
+package com.bet.betApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetAppApplication.class, args);
+	}
+}
